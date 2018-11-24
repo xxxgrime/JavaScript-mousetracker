711c3c65af36ca1b6973faa2fce149542d5e51c0
xxxgrime <mxer12390@gmail.com>
2018-11-24 18:00:54 +0530

    init repo

---
+++ index.js
@@ -0,0 +1,44 @@
+var express=require("express")
+var cors=require("cors")
+var path=require("path")
+var bodyParser=require("body-parser")
+var ejs=require("ejs")
+
+var app=express();
+
+const port =3000;
+
+
+app.use(cors())
+
+app.set("views",path.join(__dirname,"views"))
+app.set("view-engine",ejs)
+app.engine('html',require('ejs').renderFile)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+app.get('/render',function(req,res,next){
+
+  res.render("index.html")
+ 
+})
+
+var render;
+app.post('/render',(req,res,next)=>{
+    render=req.body;
+    res.send("received")
+})
+
+app.get("/getdata",(req,res,next)=>{
+
+    console.log("this is redner")
+    console.log(render)
+    render?res.send(render):res.send("render not found")
+})
+
+app.listen(port,()=>{
+    console.log("server started")
+})
